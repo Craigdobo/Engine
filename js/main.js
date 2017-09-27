19c8555d218e1ef6df838b4c39883def266926e5
Melathron <G33kg@m3s>
2017-09-27 21:32:21 +0100

    spin and stake updated

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ function init() {
         });
 
         PIXI.loader
-            .add(["img/background.png", "img/buttons/home.png", "img/quitgame.png", "img/buttons/no.png", "img/buttons/yes.png"])
+            .add(["img/background.png", "img/buttons/home.png", "img/quitgame.png", "img/buttons/no.png", "img/buttons/yes.png",
+            "img/buttons/spin.png", "img/buttons/stakefield.png", "img/buttons/up.png", "img/buttons/down.png"])
             .on("complete", assetLoad)
             .load();
 
@@ -86,7 +87,25 @@ function assetLoad() {
         refresh();
     };
 
-    stage.addChild(background, homeBtn);
+    var spin = new PIXI.Sprite(PIXI.loader.resources["img/buttons/spin.png"].texture);
+    spin.scale.set(renderer.width / 3000, renderer.width / 3000);
+    spin.position.set(renderer.width / 1.2, renderer.height / 2.2);
+    spin.interactive = true;
+    spin.buttonMode = true;
+
+    var stake = new PIXI.Sprite(PIXI.loader.resources["img/buttons/stakefield.png"].texture);
+    stake.scale.set(renderer.width / 3500, renderer.width / 3500);
+    stake.position.set(renderer.width / 50 , renderer.height / 2);
+
+    var up = new PIXI.Sprite(PIXI.loader.resources["img/buttons/up.png"].texture);
+    up.scale.set(renderer.width / 3500, renderer.width / 3500);
+    up.position.set(renderer.width / 35 , renderer.height / 2.5);
+
+    var down = new PIXI.Sprite(PIXI.loader.resources["img/buttons/down.png"].texture);
+    down.scale.set(renderer.width / 3500, renderer.width / 3500);
+    down.position.set(renderer.width / 35 , renderer.height / 1.55);
+
+    stage.addChild(background, homeBtn, spin, stake, up, down);
     refresh();
 
 }
